12625efaeaa41fc0423444a5f5f44141ef58ed1e
Manoel NTI <desenvolvimentonti@unimedcaruaru.com.br>
2024-07-10 16:51:28 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,55 @@
+﻿using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.GetById;
+using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase 
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody]RequestRegisterTripJson request) 
+        {
+            try
+            {
+                var useCase = new RegisterTripUseCase();
+
+                var response = useCase.Execute(request);
+
+                return Created(string.Empty, response);
+            }
+            catch(JourneyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro desconhecido");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var UseCase = new GetAllTripsUseCase();
+
+            var result = UseCase.Execute(); 
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute]Guid id)
+        {
+            var useCase = new GetTripByIdUseCase();
+
+            var response = useCase.Execute(id);
+
+            return Ok(response);
+        }
+    }
+}
